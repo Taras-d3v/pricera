778b8d146a73d1eb8aa20d8ad5ab9ff8d8fdff61
TarasIvaniuk <181894998+Taras-d3v@users.noreply.github.com>
2025-11-18 23:52:39 +0200

    Add new spider params: s3_bucket, s3_prefix

--- common/pricera/common/collectors/base_collector.py
+++ common/pricera/common/collectors/base_collector.py
@@ -9,7 +9,9 @@ class ScrapyConfigurationMixin:
     def __init__(self):
         self.spider_instance = None
 
-    def process_scrapy_spider(self, spider_cls, urls: list[str], proxy_config=None, **kwargs):
+    def process_scrapy_spider(
+        self, spider_cls, urls: list[str], s3_bucket: str, s3_prefix: str, proxy_config=None, **kwargs
+    ):
         process = CrawlerProcess(get_project_settings())
         crawler = process.create_crawler(spider_cls)
 
@@ -18,7 +20,9 @@ class ScrapyConfigurationMixin:
 
         crawler.signals.connect(handle_spider_opened, signal=signals.spider_opened)
 
-        process.crawl(crawler, start_urls=urls, proxy_config=proxy_config, **kwargs)
+        process.crawl(
+            crawler, start_urls=urls, proxy_config=proxy_config, s3_bucket=s3_bucket, s3_prefix=s3_prefix, **kwargs
+        )
 
         process.start()  # блокирует выполнение до конца работы паука
 
