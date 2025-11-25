7ccf2e51893fe28af379c666ec73b4451b99ff7e
TarasIvaniuk <181894998+Taras-d3v@users.noreply.github.com>
2025-11-25 19:53:25 +0200

    Add crawler pipeline

---
+++ common/pricera/common/pipelines/crawler_pipeline.py
@@ -0,0 +1,20 @@
+from typing import Callable
+from pricera.common import FileBasedMessageConsumer, MessageProcessor, RabbitMQ, get_file_args
+
+
+def crawler_pipeline(factory, message: dict) -> None:
+    collector = factory.get_crawler(message=message)
+    collector.crawl()
+
+
+def launch_crawler(factory, pipeline: Callable = crawler_pipeline, queue: str = "crawler_queue") -> None:
+    message_processor = MessageProcessor(pipeline=pipeline, factory=factory)
+    args = get_file_args()
+    if args.file:
+        file_consumer: FileBasedMessageConsumer = FileBasedMessageConsumer(
+            function=message_processor.process, file_path=args.file
+        )
+        file_consumer.consume()
+    else:
+        rabbitmq_consumer: RabbitMQ = RabbitMQ()
+        rabbitmq_consumer.consume(function=message_processor.process, queue=queue)
