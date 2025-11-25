d7f5ca6cb6106224173a37a1d8aed3b9037a21d7
TarasIvaniuk <181894998+Taras-d3v@users.noreply.github.com>
2025-11-25 19:51:08 +0200

    Remove unused files

--- common/pricera/common/middlewares/request_chain_pipeline.py
+++
@@ -1,6 +0,0 @@
-from pricera.models import ResponseObject
-
-
-class RequestChainItemPipeline:
-    def process_item(self, item: ResponseObject, spider):
-        spider.responses[item.chain_uuid].append(item)
