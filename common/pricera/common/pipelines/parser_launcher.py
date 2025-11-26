d58fcdf35e1edb958ae08a606fae01ea0be99c81
TarasIvaniuk <181894998+Taras-d3v@users.noreply.github.com>
2025-11-26 23:59:35 +0200

    Add HotlineItemCardCollector to parser routing

--- common/pricera/common/pipelines/parser_launcher.py
+++ common/pricera/common/pipelines/parser_launcher.py
@@ -1,8 +1,10 @@
 from pricera.rozetka.rozetka_product_collector import RozetkaProductCollector
+from pricera.hotline.hotline_item_card_collector import HotlineItemCardCollector
 
 # explicitly defined payload key to crawler mapping:
 PAYLOAD_KEY_TO_PARSER = {
     RozetkaProductCollector.payload_key: RozetkaProductCollector,
+    HotlineItemCardCollector.payload_key: HotlineItemCardCollector,
 }
 
 
