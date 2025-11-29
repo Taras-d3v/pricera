3c487b1c2ff67ce23e268df52492eb35450a5778
TarasIvaniuk <181894998+Taras-d3v@users.noreply.github.com>
2025-11-29 15:40:00 +0200

    Pre-commit update

--- common/pricera/common/logger/__init__.py
+++ common/pricera/common/logger/__init__.py
@@ -5,12 +5,13 @@ import os
 
 logger = logging.getLogger("logger_init")
 
+
 def set_logger():
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    config_path = os.path.join(current_dir, 'logging.yaml')
+    config_path = os.path.join(current_dir, "logging.yaml")
 
     try:
-        with open(config_path, 'r') as f:
+        with open(config_path, "r") as f:
             config = yaml.safe_load(f)
         logging.config.dictConfig(config)
         logger.info("Logger configuration loaded successfully")
