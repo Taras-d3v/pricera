119a3b113db1979393b1a74eadbf68ec7929d43a
TarasIvaniuk <181894998+Taras-d3v@users.noreply.github.com>
2025-12-02 17:58:50 +0200

    Update package imports

--- common/pricera/common/__init__.py
+++ common/pricera/common/__init__.py
@@ -3,8 +3,10 @@ __all__ = [
     "FileBasedMessageConsumer",
     "BaseCollector",
     "load_file_from_sub_folder",
+    "ensure_list"
 ]
 
 
 from .collectors import BaseCollector, FileBasedMessageConsumer, RabbitMQ
 from .testing_utilities import load_file_from_sub_folder
+from .utilities import ensure_list
