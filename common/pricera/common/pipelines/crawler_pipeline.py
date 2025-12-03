655bd285b8d71c9a923c979800082cbe870a2a37
TarasIvaniuk <181894998+Taras-d3v@users.noreply.github.com>
2025-12-04 00:30:44 +0200

    Refactor crawler_pipeline

--- common/pricera/common/pipelines/crawler_pipeline.py
+++ common/pricera/common/pipelines/crawler_pipeline.py
@@ -1,21 +1,14 @@
 from pricera.common.pipelines.collector_mapping import PAYLOAD_KEY_TO_CRAWLER
+from logging import getLogger
+from pricera.common.pipelines.utilities import prepare_message
+
+logger = getLogger("crawler_pipeline")
 
 
-class CrawlerFactory:
-    @staticmethod
-    def get_message_key_from_message(message: dict) -> str:
-        message_payload = message["payload"]
-        # todo: handle multiple keys in payload
-        message_key = list(message_payload.keys())[0]
-        return message_key
-
-    @classmethod
-    def get_crawler(cls, message: dict):
-        message_key = cls.get_message_key_from_message(message)
-        crawler_cls = PAYLOAD_KEY_TO_CRAWLER[message_key]
-        return crawler_cls.get_crawler(message=message)
-
-
-def crawler_pipeline(message: dict, factory=CrawlerFactory) -> None:
-    collector = factory.get_crawler(message=message)
-    collector.crawl()
+def crawler_pipeline(message: dict, trigger_to_cls_mapping=PAYLOAD_KEY_TO_CRAWLER) -> None:
+    for item in prepare_message(original_message=message, collector_mapping=trigger_to_cls_mapping):
+        if not item:
+            continue
+        crawler_cls, message = item
+        crawler_cls_obj = crawler_cls.get_crawler(message=message)
+        crawler_cls_obj.crawl()
