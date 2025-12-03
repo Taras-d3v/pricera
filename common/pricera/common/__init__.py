7cf21166cc9575334b9e2e72e892db14048f6fd7
TarasIvaniuk <181894998+Taras-d3v@users.noreply.github.com>
2025-12-03 23:12:19 +0200

    Update package imports with get_mongo_client function

--- common/pricera/common/__init__.py
+++ common/pricera/common/__init__.py
@@ -1,6 +1,14 @@
-__all__ = ["RabbitMQ", "FileBasedMessageConsumer", "BaseCollector", "load_file_from_sub_folder", "ensure_list"]
+__all__ = [
+    "RabbitMQ",
+    "FileBasedMessageConsumer",
+    "BaseCollector",
+    "load_file_from_sub_folder",
+    "ensure_list",
+    "get_mongo_client",
+]
 
 
 from .collectors import BaseCollector, FileBasedMessageConsumer, RabbitMQ
 from .testing_utilities import load_file_from_sub_folder
 from .utilities import ensure_list
+from .mongodb import get_mongo_client
