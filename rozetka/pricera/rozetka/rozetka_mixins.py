d9cf7e0858e6e5b6ce3288454837088d95211a30
TarasIvaniuk <181894998+Taras-d3v@users.noreply.github.com>
2025-12-06 01:54:09 +0200

    Add collection_name, remove storage_bucket attributes

--- rozetka/pricera/rozetka/rozetka_mixins.py
+++ rozetka/pricera/rozetka/rozetka_mixins.py
@@ -3,6 +3,6 @@ from typing import ClassVar
 
 class RozetkaProductMixin:
     payload_key: ClassVar[str] = "rozetka_product"
-    storage_bucket: ClassVar[str] = "pricera-crawled-data"
     storage_prefix: ClassVar[str] = "rozetka_product/"
+    collection_name: ClassVar[str] = "rozetka_product"
     collector_name: ClassVar[str] = "rozetka_product"
