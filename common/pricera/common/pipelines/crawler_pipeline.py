f2ea080b11a3d0eb54e3852fcdcddb078c404de3
TarasIvaniuk <181894998+Taras-d3v@users.noreply.github.com>
2025-12-05 20:45:26 +0200

    Fix variable shadowing in loop

--- common/pricera/common/pipelines/crawler_pipeline.py
+++ common/pricera/common/pipelines/crawler_pipeline.py
@@ -5,8 +5,8 @@ from pricera.common.pipelines.utilities import prepare_message
 logger = getLogger("crawler_pipeline")
 
 
-def crawler_pipeline(message: dict, trigger_to_cls_mapping=PAYLOAD_KEY_TO_CRAWLER) -> None:
-    for item in prepare_message(original_message=message, collector_mapping=trigger_to_cls_mapping):
+def crawler_pipeline(original_message: dict, trigger_to_cls_mapping=PAYLOAD_KEY_TO_CRAWLER) -> None:
+    for item in prepare_message(original_message=original_message, collector_mapping=trigger_to_cls_mapping):
         if not item:
             continue
         crawler_cls, message = item
