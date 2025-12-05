76c391b6d5d17f3960d661063a047df077a8f32c
TarasIvaniuk <181894998+Taras-d3v@users.noreply.github.com>
2025-12-05 21:06:31 +0200

    Add collector_name cls variable

--- rozetka/pricera/rozetka/rozetka_mixins.py
+++ rozetka/pricera/rozetka/rozetka_mixins.py
@@ -5,3 +5,4 @@ class RozetkaProductMixin:
     payload_key: ClassVar[str] = "rozetka_product"
     storage_bucket: ClassVar[str] = "pricera-crawled-data"
     storage_prefix: ClassVar[str] = "rozetka_product/"
+    collector_name: ClassVar[str] = "rozetka_product"
