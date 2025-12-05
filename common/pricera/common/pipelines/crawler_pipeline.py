55b9dcb4b5941f56146fbebd2d84d267fd30e1b1
TarasIvaniuk <181894998+Taras-d3v@users.noreply.github.com>
2025-12-05 21:06:48 +0200

    Fix list of variables

--- common/pricera/common/pipelines/crawler_pipeline.py
+++ common/pricera/common/pipelines/crawler_pipeline.py
@@ -1,14 +1,15 @@
 from pricera.common.pipelines.collector_mapping import PAYLOAD_KEY_TO_CRAWLER
 from logging import getLogger
 from pricera.common.pipelines.utilities import prepare_message
+from pymongo import MongoClient
 
 logger = getLogger("crawler_pipeline")
 
 
-def crawler_pipeline(original_message: dict, trigger_to_cls_mapping=PAYLOAD_KEY_TO_CRAWLER) -> None:
-    for item in prepare_message(original_message=original_message, collector_mapping=trigger_to_cls_mapping):
+def crawler_pipeline(mongo_client: MongoClient, message: dict, trigger_to_cls_mapping=PAYLOAD_KEY_TO_CRAWLER) -> None:
+    for item in prepare_message(message=message, collector_mapping=trigger_to_cls_mapping):
         if not item:
             continue
-        crawler_cls, message = item
-        crawler_cls_obj = crawler_cls.get_crawler(message=message)
+        crawler_cls, prepared_message = item
+        crawler_cls_obj = crawler_cls.get_crawler(message=prepared_message, mongo_client=mongo_client)
         crawler_cls_obj.crawl()
