4541318b63856d802d02fabfb849487a8c6a5886
TarasIvaniuk <181894998+Taras-d3v@users.noreply.github.com>
2025-12-06 05:01:23 +0200

    Fix RozetkaProductMixin`s storage_prefix attribute

--- rozetka/pricera/rozetka/rozetka_mixins.py
+++ rozetka/pricera/rozetka/rozetka_mixins.py
@@ -3,6 +3,6 @@ from typing import ClassVar
 
 class RozetkaProductMixin:
     payload_key: ClassVar[str] = "rozetka_product"
-    storage_prefix: ClassVar[str] = "rozetka_product/"
+    storage_prefix: ClassVar[str] = "rozetka_product"
     collection_name: ClassVar[str] = "rozetka_product"
     collector_name: ClassVar[str] = "rozetka_product"
